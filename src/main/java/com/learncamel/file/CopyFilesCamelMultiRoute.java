d0db3f293ba6654d568107afc9aef9954f085c59
Sander <sander.trap@paazl.com>
2019-02-04 22:25:50 +0100

    Added a class that showcases multiple Camel routes

--- src/main/java/com/learncamel/file/CopyFilesCamelMultiRoute.java
+++ src/main/java/com/learncamel/file/CopyFilesCamelMultiRoute.java
@@ -4,7 +4,7 @@ import org.apache.camel.CamelContext;
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.impl.DefaultCamelContext;
 
-public class CopyFilesCamelLogging {
+public class CopyFilesCamelMultiRoute {
 
     public static void main(String[] args) {
 
@@ -15,7 +15,12 @@ public class CopyFilesCamelLogging {
                 public void configure() throws Exception {
                     from("file:data/input?noop=true")
                             .to("log:?level=INFO&showBody=true&showHeaders=true")
-                            .to("file:data/output");
+                            .to("file:data/output")
+                            .to("file:data/another-output");
+
+
+                    from("file:data/input1?noop=true")
+                            .to("file:data/output1");
                 }
             });
 
