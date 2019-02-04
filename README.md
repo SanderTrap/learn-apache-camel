5b542d6336843b83aa4c7cfc3b9b949b047b8f4d
Sander <sander.trap@paazl.com>
2019-02-04 21:51:34 +0100

    Created simple camel route with Java comparison

---
+++ README.md
@@ -0,0 +1,4 @@
+# Simple Camel Route
+This project is part of a Udemy course called [Apache Camel for Beginners - Learn by Coding in Java](https://www.udemy.com/apache-camel-for-beginners-learn-by-coding-in-java/)
+
+This project is meant to learn how to create a simple Apache Camel route.
---
+++ src/main/java/com/learncamel/file/CopyFilesWithoutCamel.java
@@ -0,0 +1,41 @@
+package com.learncamel.file;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+
+public class CopyFilesWithoutCamel {
+
+    public static void main(String[] args) throws IOException {
+
+        // Step 1: Create a file Object to read the directories.
+        File inputDirectory = new File("data/input");
+        File outputDirectory = new File("data/output");
+
+        // Step 2: Read the files from the directory and iterate the files.
+        File[] files = inputDirectory.listFiles();
+
+        for (File source : files) {
+            if (source.isFile()) {
+                File dest = new File (
+                        outputDirectory.getPath()
+                                + File.separator
+                                + source.getName());
+
+                // Step 3: Create the output stream and read the files.
+                OutputStream oStream = new FileOutputStream(dest);
+                byte[] buffer = new byte[(int) source.length()];
+                FileInputStream iStream = new FileInputStream(source);
+                try {
+                    oStream.write(buffer);
+                } finally {
+                    // Step 4: Close the streams.
+                    oStream.close();
+                    iStream.close();
+                }
+            }
+        }
+    }
+}
