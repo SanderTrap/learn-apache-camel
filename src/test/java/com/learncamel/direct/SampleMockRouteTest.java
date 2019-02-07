b0ae5af0e39d5b3f17dfac7180ed9e8c8dca15e5
Sander <sander.trap@paazl.com>
2019-02-07 07:30:54 +0100

    Created a new Route with a Mock Component and also created a test with this Route

---
+++ src/test/java/com/learncamel/direct/SampleMockRouteTest.java
@@ -0,0 +1,26 @@
+package com.learncamel.direct;
+
+import org.apache.camel.RoutesBuilder;
+import org.apache.camel.component.mock.MockEndpoint;
+import org.apache.camel.test.junit4.CamelTestSupport;
+import org.junit.Test;
+
+public class SampleMockRouteTest extends CamelTestSupport {
+
+    @Override
+    protected RoutesBuilder createRouteBuilder() throws Exception {
+        return new SampleMockRoute();
+    }
+
+    @Test
+    public void sampleMockRouteTest() throws InterruptedException {
+        String input = "Hello";
+
+        MockEndpoint mock = getMockEndpoint("mock:output");
+        mock.expectedBodiesReceived(input);
+
+        template.sendBody("direct:sampleInput", input);
+
+        assertMockEndpointsSatisfied();
+    }
+}
