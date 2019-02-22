d429d151d8c7ad59abfeace467a9d1abb3c33253
Sander <sander.trap@paazl.com>
2019-02-22 06:19:24 +0100

    Unmarshal CSV using Link

---
+++ src/test/java/com/learncamel/routes/csv/CsvUnmarshalWithLinkRouteTest.java
@@ -0,0 +1,27 @@
+package com.learncamel.routes.csv;
+
+import com.learncamel.domain.EmployeeWithAddress;
+import org.apache.camel.Exchange;
+import org.apache.camel.RoutesBuilder;
+import org.apache.camel.test.junit4.CamelTestSupport;
+import org.junit.Test;
+
+public class CsvUnmarshalWithLinkRouteTest extends CamelTestSupport {
+
+    @Override
+    protected RoutesBuilder createRouteBuilder() throws Exception {
+        return new CsvUnmarshalWithLinkRoute();
+    }
+
+    @Test
+    public void unMarshalCsvTest() throws InterruptedException {
+        Exchange exchange = consumer.receive("direct:output");
+
+        Thread.sleep(3000);
+
+        EmployeeWithAddress employeeWithAddress = (EmployeeWithAddress) exchange.getIn().getBody();
+        System.out.println(employeeWithAddress);
+
+        assertEquals("USA", employeeWithAddress.getAddress().getCountry());
+    }
+}
