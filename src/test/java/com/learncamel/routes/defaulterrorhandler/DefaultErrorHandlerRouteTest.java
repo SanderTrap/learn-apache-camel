5329ce34657afe3c686ba25cbd0715e414f607d3
Sander <sander.trap@paazl.com>
2019-02-23 17:21:40 +0100

    Apache Camel Default Error Handler

---
+++ src/test/java/com/learncamel/routes/defaulterrorhandler/DefaultErrorHandlerRouteTest.java
@@ -0,0 +1,23 @@
+package com.learncamel.routes.defaulterrorhandler;
+
+import org.apache.camel.RoutesBuilder;
+import org.apache.camel.test.junit4.CamelTestSupport;
+import org.junit.Test;
+
+public class DefaultErrorHandlerRouteTest extends CamelTestSupport {
+
+    @Override
+    protected RoutesBuilder createRouteBuilder() throws Exception {
+        return new DefaultErrorHandlerRoute();
+    }
+
+    @Test(expected = RuntimeException.class)
+    public void exceptionCheck() throws InterruptedException {
+        String expectedOutput = "123*Sander*23FEB19";
+        String input = null;
+
+        String output = template.requestBody("direct:exception", input, String.class);
+
+        assertEquals(expectedOutput, output);
+    }
+}
