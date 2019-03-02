ce1e8542ab0397bc42c749633b6714327ef24c63
Sander <sander.trap@paazl.com>
2019-03-02 21:19:46 +0100

    Connect to ActiveMQ using Apache Camel

---
+++ learn-camel-activemq2db/src/main/java/com/learncamel/jms/JmsReadRoute.java
@@ -0,0 +1,13 @@
+package com.learncamel.jms;
+
+import org.apache.camel.builder.RouteBuilder;
+
+public class JmsReadRoute extends RouteBuilder {
+
+    public void configure() throws Exception {
+        from("activemq:queue:testQueue?username=admin&password=admin")
+                .streamCaching()
+                .to("log:?level=INFO&showBody=true")
+                .to("direct:readQueue");
+    }
+}
